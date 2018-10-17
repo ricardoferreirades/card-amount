edcbfeeb002c770584de72498372070c8d8d5ed5
Ricardo Ferreira <ricardo.ferreira@justa.com.vc>
2018-10-16 21:00:01 -0300

    adding git ignore, adding jquery, cretating example, adding new features, removing click method

--- card-amount.js
+++ card-amount.js
@@ -1,42 +1,53 @@
 // constructor
-var CardAmount = function(selector) {
-    this.selector = $(selector);
-    this.click();
+function CardAmount (options) {
+    this.selector = $(options.card);
+    this.title = $(options.title);
+    this.amountQuantity = $(options.amountQuantity);
+    this.titleValue = options.titleValue;
+    this.data = $(options.data);
+    this.init();
 }
 
 /*
    - method to show/hide card
    - render content
 */
-CardAmount.prototype.setCardData = function(data, qtd) {
+CardAmount.prototype.setCardData = function(data = 0, qtd = 0) {
     /*
       - check if has content to render
       - show/hide/close
     */
-    if(!isNaN(data) && data > 0) {
-        if(this.selector.hasClass('hide')) {
-            this.selector.removeClass("hide");
-            this.selector.addClass("opened");
-        }
+
+    if(qtd) {
+        this.selector.removeClass("hide");
+        this.selector.addClass("opened");
+        
     } else {
         this.selector.removeClass("opened closed");
         this.selector.addClass("hide");
     }
 
+    data = data.toFixed(2);
+
+    amount = data.toString();
+    var dot = amount.lastIndexOf('.');
+
+    if (dot != -1) {
+        amount = amount.substr(0, dot) + "," + amount.substr(dot + 1);
+    }
+
     // render content
-    this.selector.html("<h4 class='title'>Soma dos selecionados" + qtd + "</h4><p>R$ "+ data.toFixed(2)+"</p>");
+    this.data.html(parseFloat(data) > 0 ? 'R$' + amount : null);
+    this.amountQuantity.html(qtd > 0 ? ''+ qtd : null);
 };
 
-// click method
-CardAmount.prototype.click = function() {
-    //click event
-    this.selector.on("click", function () {
-        var $target = $(this);
 
-        if($target.hasClass('opened')) {
-            $target.toggleClass(('opened closed'));
-        } else {
-            $target.toggleClass(('opened closed'));
-        }
-    });
+// click method
+CardAmount.prototype.init = function() {
+    //click event
+    $(this.selector).addClass('hide card-amount');
+    $(this.title).addClass('card-amount__title');
+    $(this.title).text(this.titleValue);
+    $(this.amountQuantity).addClass('card-amount__amount-quantity');
+    $(this.data).addClass('card-amount__amount');
 };
